2dcbc5e18b9c4d58a88f7a40249c7fc6f6d3254b
Grzegorz Antoniak <ga@anadoxin.org>
2016-02-07 21:10:09 +0100

    code works

--- src/mksymbols/core.clj
+++ src/mksymbols/core.clj
@@ -1,10 +1,12 @@
 (ns mksymbols.core
   (:require [clojure.tools.cli :refer [parse-opts]]
-            [clojure.java.io   :as    io])
+            [clojure.java.io   :as    io]
+            [clojure.java.shell :as   shell])
 
   (:gen-class))
 
 (def glob-dumpsym-path "/home/antek/temp/build/crashes/dump_syms")
+(def glob-symbols-path "/home/antek/temp/build/crashes/symbols")
 
 ;; https://github.com/clojure/tools.cli
 (def cli-options [
@@ -23,14 +25,54 @@
 
 (defn is-elf [path] (= 0x7f454c46 (read-4 path)))
 
+(defn extract-buildid-from-headline [headline]
+    (let [match (re-find #".* ([0-9a-fA-F]+) .*" headline)]
+        (if (> (count match) 1)
+            (match 1)
+            nil)))
+
+(defn extract-buildid [symdata]
+    (let [headline (.readLine (java.io.BufferedReader. (java.io.StringReader. (.toString symdata))))]
+        (extract-buildid-from-headline headline)))
+
+(defn do-move-symdata [args]
+    (let [symdata (args :symdata)
+          buildid (args :buildid)
+          abspath (args :abspath)]
+        (let [new-sym-file (java.io.File. (format "%s/%s/%s"
+                                                  glob-symbols-path
+                                                  (.getFileName abspath)
+                                                  buildid))]
+            (do
+                (if (false? (.isDirectory new-sym-file))
+                    (if (false? (.mkdirs new-sym-file))
+                        (do
+                            (println (format "Can't create directory for %s: %s" (.toString abspath) (.getPath new-sym-file)))
+                            false)))
+                (do
+                    (spit (format "%s/%s/%s/%s.sym" glob-symbols-path (.getFileName abspath) buildid (.getFileName abspath))
+                          symdata))))))
+
 (defn do-process-elf [path]
-    (let [dumpsym-cmdline (format "%s %s 2> /dev/null >/tmp/%s.sym" glob-dumpsym-path path "basename")]
-        (println dumpsym-cmdline)
-        true))
+    (let [abspath (.normalize (.toPath (io/file path)))]
+        (do
+            (println (format "Processing ELF file %s" (.toString abspath)))
+
+            (let [symdata (clojure.java.shell/sh glob-dumpsym-path (.toString abspath))]
+                (let [buildid (extract-buildid (symdata :out))]
+                    (if (= nil buildid)
+                        (do
+                            (println (format "Can't process ELF: %s" path))
+                            false)
+                        (do-move-symdata {:symdata (symdata :out)
+                                          :buildid buildid
+                                          :abspath abspath})))))))
 
 (defn process-file [path]
     (if (true? (is-elf path))
         (do-process-elf path)))
+
+(declare process-dir)
 
 (defn process-item [fileobj]
     (let [filepath (.getPath fileobj)]
